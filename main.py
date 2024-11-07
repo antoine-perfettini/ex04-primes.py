d5b1408e0de6cbe55cf77b258a5dfb802a33dc02
antoine-perfettini <antoine.perfettini@edu.esiee.fr>
2024-11-07 11:22:13 +0000

    Mon code permet de déterminer si un nombre est premier ou pas

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-from math import sqrt
-
+#from math import sqrt
 #### Fonction secondaire
+"""
+Permet de déterminer si un entier est premier
 
-
+"""
 def isprime(p):
+    """
+    Fonction qui permet de déterminer si un entier est premier
+    """
 
     # votre code ici
+    #premier = True
+    #p = int(input("Entrer votre nombre:"))
+    #RCp = int(math.sqrt(p))
+    if p<=1 :
+        return False
+    for i in range(2, p-1):
+        if p%i == 0:
+            return False
 
-    pass
+
+    return True
+
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Fonction principale
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
         if isprime(n):
-            print(n, end=", ")
-
-    print()
-
+            print(n, end=",")
+        #print()
 
 if __name__ == "__main__":
     main()
